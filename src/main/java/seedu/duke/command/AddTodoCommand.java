137eed09a155f867fb01c30b424f858287fd67f6
hau shuan <huanghaushuan@gmail.com>
2026-03-18 00:04:43 +0800

    Apply Logging to Add command

--- src/main/java/seedu/duke/command/AddTodoCommand.java
+++ src/main/java/seedu/duke/command/AddTodoCommand.java
@@ -1,5 +1,6 @@
 package seedu.duke.command;
 
+import java.util.logging.Logger;
 import seedu.duke.exception.ModuleSyncException;
 import seedu.duke.module.Module;
 import seedu.duke.module.ModuleBook;
@@ -8,6 +9,7 @@ import seedu.duke.task.Task;
 import seedu.duke.ui.Ui;
 
 public class AddTodoCommand extends Command {
+    private static final Logger LOGGER = Logger.getLogger(AddTodoCommand.class.getName());
     private final String moduleCode;
     private final String description;
 
@@ -20,9 +22,11 @@ public class AddTodoCommand extends Command {
 
     @Override
     public void execute(ModuleBook moduleBook, Storage storage, Ui ui) throws ModuleSyncException {
+        LOGGER.fine(() -> "Adding todo to module " + moduleCode);
         Module module = moduleBook.getOrCreate(moduleCode);
         Task task = module.addTodo(description);
         storage.save(moduleBook);
+        LOGGER.fine(() -> "Todo added and saved for module " + moduleCode);
         ui.showTaskAdded(module, task, moduleBook.totalTaskCount());
     }
 }
